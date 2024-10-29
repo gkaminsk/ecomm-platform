5d92ea68dde0f13ede2508c003ed08b379f56d79
gk-dev-studio <gk.dev.studio@outlook.com>
2024-10-30 00:19:22 +0100

    Changing consol projects to wepapps

--- eComm.Platform.EventFactory/Program.cs
+++ eComm.Platform.EventFactory/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace BackgroundWorker
+using SharedLibrary.ExceptionHandlers;
+
+namespace eComm.Platform.EventFactory
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+            builder.Services.AddProblemDetails();
+            builder.Services.AddHealthChecks();
+
+
+            var app = builder.Build();
+
+            app.UseHttpsRedirection();
+            app.MapHealthChecks("health");
+
+            app.MapGet("", (HttpContext httpContext) =>
+            {
+                var result = new
+                {
+                    Service = typeof(Program).Assembly.GetName().Name,
+                    @Version = typeof(Program).Assembly.GetName()?.Version?.ToString(),
+                    Date = $"{DateTime.UtcNow.ToString("yyyy-MM-dd hh:mm:ss")} UTC",
+                };
+
+                return result;
+            })
+            .WithOpenApi();
+
+            app.UseExceptionHandler();
+
+            app.Run();
         }
     }
 }
